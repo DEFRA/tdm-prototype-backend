d1a522737fe5fffd0bbd8a3511cddc7a2a0be406
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2024-10-18 13:18:10 +0100

    updated relationships and added to schema

--- TdmPrototypeBackend.Api/Swagger/NotificationResourceResponse.cs
+++ TdmPrototypeBackend.Api/Swagger/NotificationResourceResponse.cs
@@ -1,4 +1,6 @@
+using JsonApiDotNetCore.Resources.Annotations;
 using TdmPrototypeBackend.Types.Alvs;
+using TdmPrototypeBackend.Types.Extensions;
 using TdmPrototypeBackend.Types.Ipaffs;
 
 namespace TdmPrototypeBackend.Api.Swagger
@@ -6,5 +8,91 @@ namespace TdmPrototypeBackend.Api.Swagger
     public class NotificationResourceResponse : ResourceResponse<Notification>
     {
 
+    }
+
+    public class JsonApiTdmRelationshipMeta
+    {
+        [Attr]
+        public bool Matched { get; set; } = default!;
+    }
+
+    public class TdmDataMeta 
+    {
+        [Attr]
+        public bool Matched { get; set; } = default!;
+
+        [Attr]
+        public string Self { get; set; }
+
+        [Attr]
+        public int? SourceItem { get; set; } = default!;
+
+        [Attr]
+        public int? DestinationItem { get; set; } = default!;
+
+        [Attr]
+        public int? MatchingLevel { get; set; }
+    }
+  
+
+    public class JsonApiTdmRelationships
+    {
+        [Attr]
+        public JsonApiTdmRelationshipObject Movements { get; set; }
+
+        
+    }
+
+    public class MovementTdmRelationships 
+    {
+        [Attr]
+        public JsonApiTdmRelationshipObject Notifications { get; set; }
+
+    }
+
+    public sealed class JsonApiTdmRelationshipObject
+    {
+        [Attr]
+        public JsonApiTdmRelationshipMeta Meta { get; set; } = default!;
+
+        [Attr]
+        public JsonApiRelationshipLinks Links { get; set; }
+
+        [Attr]
+        public List<JsonApiRelationshipDataItem> Data { get; set; } = new List<JsonApiRelationshipDataItem>();
+
+      
+    }
+
+
+    public sealed class JsonApiRelationshipLinks
+    {
+        [Attr]
+        public string Self { get; set; }
+
+        [Attr]
+        public string Related { get; set; }
+
+      
+    }
+
+    //public sealed class JsonApiResourceLink
+    //{
+    //    [Attr]
+    //    public string Self { get; set; }
+    //}
+
+    public sealed class JsonApiRelationshipDataItem
+    {
+       
+
+        [Attr]
+        public string Type { get; set; }
+
+        [Attr]
+        public string Id { get; set; }
+
+        [Attr]
+        public TdmDataMeta Meta { get; set; }
     }
 }
--- TdmPrototypeBackend.Api/Swagger/ResourceData.cs
+++ TdmPrototypeBackend.Api/Swagger/ResourceData.cs
@@ -1,8 +1,13 @@
-﻿namespace TdmPrototypeBackend.Api.Swagger;
+using JsonApiDotNetCore.Serialization.Objects;
+using TdmPrototypeBackend.Types;
+
+namespace TdmPrototypeBackend.Api.Swagger;
 
 public class ResourceData<T>
 {
     public string Type { get; set; }
     public string Id { get; set; }
     public T Attributes { get; set; }
+
+    public JsonApiTdmRelationships Relationships { get; set; }
 }
